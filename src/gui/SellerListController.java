d4f32feaec635b50afe613e101be8b7373519b41
Leosmp <leosoutomaior@.com>
2020-08-24 16:57:07 -0300

    SellerForm

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
@@ -16,7 +17,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -24,6 +27,8 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
 import model.services.SellerService;
@@ -106,27 +111,27 @@ public class SellerListController implements Initializable, DataChangeListener {
 	}
 
 	private void createDialogForm(Seller obj, String absoluteName, Stage parentStage) {
-//		try {
-//			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName)); // para carregar uma view
-//			Pane pane = loader.load();
-//
-//			SellerFormController controller = loader.getController();
-//			controller.setSeller(obj);
-//			controller.setSellerService(new SellerService());
-//			controller.subscribeDataChangeListener(this);
-//			controller.updateFormData();
-//
-//			Stage diologStage = new Stage();
-//			diologStage.setTitle("Enter Seller data");
-//			diologStage.setScene(new Scene(pane));
-//			diologStage.setResizable(false);
-//			diologStage.initOwner(parentStage);
-//			diologStage.initModality(Modality.WINDOW_MODAL);
-//			diologStage.showAndWait();
-//
-//		} catch (IOException e) {
-//			Alerts.ShowAlert("IOException", "Error loading view", e.getMessage(), AlertType.ERROR);
-//		}
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName)); // para carregar uma view
+			Pane pane = loader.load();
+
+			SellerFormController controller = loader.getController();
+			controller.setSeller(obj);
+			controller.setSellerService(new SellerService());
+			controller.subscribeDataChangeListener(this);
+			controller.updateFormData();
+
+			Stage diologStage = new Stage();
+			diologStage.setTitle("Enter Seller data");
+			diologStage.setScene(new Scene(pane));
+			diologStage.setResizable(false);
+			diologStage.initOwner(parentStage);
+			diologStage.initModality(Modality.WINDOW_MODAL);
+			diologStage.showAndWait();
+
+		} catch (IOException e) {
+			Alerts.ShowAlert("IOException", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 	@Override
